ac072e0c6278ab56496429f16027d12b15f9351d
Aigul <sharipova.aig@gmail.com>
2022-10-31 22:00:31 -0400

    delay

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //gsap.to(".header", {x: 500, y: 400, duration: 6})
 //gsap.from(".head", {x: 400, y: 300, duration: 10})
 
-gsap.to(".dog", {x: 400, y: 300, duration: 10})
+gsap.to(".cat", {x: 200, duration: 7, delay: 3})
+gsap.to(".food", {x: 200, duration: 5, delay: 1})
