5790ba61ed4792eee77de03b9005ad3d182479f3
Aigul <sharipova.aig@gmail.com>
2022-10-31 21:28:40 -0400

    image

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-gsap.to(".header", {x: 500, y: 400, duration: 6})
-gsap.from(".head", {x: 400, y: 300, duration: 10})
+//gsap.to(".header", {x: 500, y: 400, duration: 6})
+//gsap.from(".head", {x: 400, y: 300, duration: 10})
+
+gsap.to(".dog", {x: 400, y: 300, duration: 10})
